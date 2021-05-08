01229fe31a84c02f90794fc601523f90308df868
Anthony J Mainero <anthony@Anthonys-MBP.lan>
2021-05-07 21:31:42 -0700

    Added amplify auth

--- App.tsx
+++ App.tsx
@@ -1,5 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StyleSheet, Text, View } from "react-native"
+import Amplify from "aws-amplify"
+import config from "./src/aws-exports"
+
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import useCachedResources from './hooks/useCachedResources';
