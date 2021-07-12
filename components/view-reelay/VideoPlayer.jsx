312a4782a2ff537a0c0b741a7211a8abf6065fd9
Anthony J Mainero <anthony@Anthonys-MacBook-Pro.local>
2021-07-12 12:06:28 +0200

    Video player restarts properly when scrolling back through the feed, but not when switching back and forth between tabs.

--- components/view-reelay/VideoPlayer.jsx
+++ components/view-reelay/VideoPlayer.jsx
@@ -4,25 +4,55 @@ import { VideoStyles } from '../../styles';
 import { useFocusEffect } from '@react-navigation/native';
 
 export default function VideoPlayer({ videoURI, poster, isPlay }) {
-	const video = useRef(null);
 	const [playbackStatus, setPlaybackStatus] = useState({});
+	const [playbackObject, setPlaybackObject] = useState(null);
+
 	const [isFocused, setIsFocused] = useState(false);
+	const [isLoaded, setIsLoaded] = useState(false);
 
     useFocusEffect(React.useCallback(() => {
         setIsFocused(true);
-        return () => setIsFocused(false);
+
+        return () => {
+			try {
+				if (playbackObject && !isPlay) {
+					const prom = playbackObject.setPositionAsync(0)
+						.catch((error) => {
+						// todo: this throws a seeking interrupted error
+						// seems harmless, but I'm not sure
+				});
+				}
+			} catch (error) {
+				console.log(error);
+			}
+			setIsFocused(false);
+		}
     }));
 
+	const _handleVideoRef = (component) => {
+		setPlaybackObject(component);
+	}
+
 	const onPlaybackStatusUpdate = (status) => {
-		setPlaybackStatus(status);
-		
+		try {
+			setPlaybackStatus(status);
+		} catch (error) {
+			console.log(error);
+		}
+
+		if (status.isBuffering && isLoaded) {
+			setIsLoaded(false); 
+		} else if (!isLoaded) {
+			setIsLoaded(true);
+		}
+
 		if (status.didJustFinish) {
 			console.log("Playback finished");
 		}
 	}
 
 	return (
-		<Video
+			<Video
 			interruptionModeAndroid={Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX}
 			interruptionModeIOS={Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX}
 			isLooping
@@ -30,8 +60,9 @@ export default function VideoPlayer({ videoURI, poster, isPlay }) {
 			onPlaybackStatusUpdate={onPlaybackStatusUpdate}
 			playsInSilentLockedModeIOS={true}
 			posterSource={poster}
+			progressUpdateIntervalMillis={50}
 			rate={1.0}
-			ref={video}
+			ref={(component) => _handleVideoRef(component)}
 			resizeMode='cover'
 			shouldDuckAndroid={true}
 			shouldPlay={isPlay && isFocused}
