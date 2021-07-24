6a230f7e2403ace27ce1bd799339cab034d1729c
Anthony J Mainero <anthony@192.168.1.5>
2021-07-24 23:36:13 +0300

    Fixing dependency issues for the expo build

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,5 @@
-const blacklist = require('metro-config/src/defaults/blacklist');
+// const blacklist = require('metro-config/src/defaults/blacklist');
+const blacklist = require('metro-config/src/defaults/exclusionList');
 module.exports = {
   resolver: {
     blacklistRE: blacklist([/#current-cloud-backend\/.*/]),
