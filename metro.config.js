e51a9c100b5a1cb605280fe79e4c0ecd7add818f
Anthony J Mainero <anthony@Anthonys-MacBook-Pro.local>
2021-07-31 21:27:44 -0700

    Updating circleci config blacklist

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,5 @@
-// const blacklist = require('metro-config/src/defaults/blacklist');
-const blacklist = require('metro-config/src/defaults/exclusionList');
+const blacklist = require('metro-config/src/defaults/blacklist');
+// const blacklist = require('metro-config/src/defaults/exclusionList');
 module.exports = {
   resolver: {
     blacklistRE: blacklist([/#current-cloud-backend\/.*/]),
