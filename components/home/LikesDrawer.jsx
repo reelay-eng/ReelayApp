adc74a96d7afbe5ecf52f52cc98bc8ae01f4c2b9
Anthony J Mainero <anthony@Anthonys-MBP.lan>
2021-09-16 18:53:59 -0700

    And now the modal for seeing who liked a reelay looks usable

--- components/home/LikesDrawer.jsx
+++ components/home/LikesDrawer.jsx
@@ -1,23 +1,42 @@
 import React, { useContext, useEffect, useRef, useState } from 'react';
-import { Dimensions, Modal, View, Text } from 'react-native';
-import { BottomSheet, Button } from 'react-native-elements';
+import { Dimensions, Modal, View, Text, Pressable } from 'react-native';
+import { Button, Icon } from 'react-native-elements';
 import { VisibilityContext } from '../../context/VisibilityContext';
 import styled from 'styled-components/native';
 
 const { height, width} = Dimensions.get('window');
 
 export default LikesDrawer = ({ reelay }) => {
-
+    const CLOSE_BUTTON_SIZE = 36;
     // https://medium.com/@ndyhrdy/making-the-bottom-sheet-modal-using-react-native-e226a30bed13
+    const Backdrop = styled(Pressable)`
+        background-color: black;
+        height: 100%;
+        width: 100%;
+        opacity: 0.4
+    `
+    const Header = styled(View)`
+        align-items: center;
+        flex-direction: row;
+        justify-content: space-between;
+        margin: 12px;
+    `
+    const HeaderText = styled(Text)`
+        font-family: System;
+        font-size: 20px;
+        font-weight: 500;
+        color: white;
+    `
     const LikesContainer = styled(View)`
         background-color: black;
-        height: 50%;
+        border-top-left-radius: 12px;
+        border-top-right-radius: 12px;
+        height: auto;
         margin-top: auto;
+        padding-bottom: 80px;
         width: 100%;
     `
     const LikeItemContainer = styled(View)`
-        border-color: white;
-        border-width: 1px;
         margin: 10px;
         width: 100%;
     `
@@ -28,16 +47,23 @@ export default LikesDrawer = ({ reelay }) => {
     `
     const visibilityContext = useContext(VisibilityContext);
     const visible = visibilityContext.likesVisible;
+    const closeDrawer = () => visibilityContext.setLikesVisible(false);
 
     return (
-        <Modal animationType='slide' transparent={true} visible={visible} onRequestClose={() => visibilityContext.setLikesVisible(false)} >
+        <Modal animationType='slide' transparent={true} visible={visible} onRequestClose={closeDrawer} >
             <LikesContainer>
-                <UsernameText>{'Likes'}</UsernameText>
-                <Button title='Close' type='solid' onPress={() => visibilityContext.setLikesVisible(false)} />
+                <Header>
+                    <HeaderText>{'Likes'}</HeaderText>
+                    <Pressable onPress={closeDrawer}>
+                        <Icon color={'white'} type='ionicon' name='close' size={CLOSE_BUTTON_SIZE} />
+                    </Pressable>
+                </Header>
                 { reelay.likes.map(like => {
-                    <LikeItemContainer key={like.userID}>
-                        <UsernameText>{like.userID}</UsernameText>
-                    </LikeItemContainer>
+                    return (
+                        <LikeItemContainer key={like.userID}>
+                            <UsernameText>{like.userID}</UsernameText>
+                        </LikeItemContainer>
+                    );
                 })}
             </LikesContainer>
         </Modal>
