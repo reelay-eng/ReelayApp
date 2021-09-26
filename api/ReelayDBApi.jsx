53817a7ebbf346eaa0b15241983a54bc42c760d1
Anthony J Mainero <anthony@Anthonys-MacBook-Pro.local>
2021-09-25 23:32:14 -0700

    First successful pass of registering a user

--- api/ReelayDBApi.jsx
+++ api/ReelayDBApi.jsx
@@ -4,12 +4,10 @@ import { fetchResults } from './fetchResults';
 const REELAY_API_BASE_URL = Constants.manifest.extra.reelayApiBaseUrl;
 
 export const getRegisteredUser = async (userID) => {
-    console.log('Fetching user...');
+    console.log('Fetching registered user...');
     const routeGet = REELAY_API_BASE_URL + '/users/' + userID;
-    console.log(routeGet);
     const resultGet = await fetchResults(routeGet, { method: 'GET' });
-
-    console.log('Fetched user result: ', resultGet);
+    console.log('Registered user result: ', resultGet);
 
     if (!resultGet) {
         console.log('User not registered');
@@ -19,21 +17,17 @@ export const getRegisteredUser = async (userID) => {
 }
 
 export const registerUser = async (user, pushToken) => {
-    console.log(user);
-    console.log(pushToken);
     const { attributes, username } = user;
     const { email, sub } = attributes;
-    console.log('will register the following user: ');
-    console.log(attributes.email, attributes.sub, username, pushToken.length);
 
-    console.log('fetching user list');
-    const routePost = REELAY_API_BASE_URL + '/users';
-    console.log(routePost);
-    const resultGet = await fetchResults(routePost, { method: 'GET' });
+    const routeGet = REELAY_API_BASE_URL + '/users/' + sub;
+    const resultGet = await fetchResults(routeGet, { method: 'GET' });
 
     if (resultGet.error) {
         // user is not registered
         console.log('Registering user...');
+        // todo: replace any + symbols in email address with %2b
+        // todo: sanity check emails and usernames
         const routePost = `${REELAY_API_BASE_URL}/users?email=${email}&username=${username}&sub=${sub}&pushToken=${pushToken}`
         const resultPost = await fetchResults(routePost, { method: 'POST' });
         console.log('User registry entry created: ', resultPost);
