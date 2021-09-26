17fdae53a5940c97ce22e238f822286eb7ced6a3
Anthony J Mainero <anthony@Anthonys-MacBook-Pro.local>
2021-09-26 09:32:29 -0700

    User auto-registration now accepts + symbols in emails

--- api/ReelayDBApi.jsx
+++ api/ReelayDBApi.jsx
@@ -23,12 +23,15 @@ export const registerUser = async (user, pushToken) => {
     const routeGet = REELAY_API_BASE_URL + '/users/' + sub;
     const resultGet = await fetchResults(routeGet, { method: 'GET' });
 
+    const encEmail = encodeURIComponent(email);
+    const encUsername = encodeURI(username);
+
     if (resultGet.error) {
         // user is not registered
         console.log('Registering user...');
         // todo: replace any + symbols in email address with %2b
         // todo: sanity check emails and usernames
-        const routePost = `${REELAY_API_BASE_URL}/users?email=${email}&username=${username}&sub=${sub}&pushToken=${pushToken}`
+        const routePost = `${REELAY_API_BASE_URL}/users?email=${encEmail}&username=${encUsername}&sub=${sub}&pushToken=${pushToken}`
         const resultPost = await fetchResults(routePost, { method: 'POST' });
         console.log('User registry entry created: ', resultPost);
     };
