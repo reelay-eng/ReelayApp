31158ba272f38c3011e94cc0df9e2a9cdf56d61b
theurul <andras.zolyomi@gmail.com>
2021-12-23 22:19:56 +0100

    Fix async notification settings bug -- react state update on unmounted component

--- components/profile/NotificationSettings.jsx
+++ components/profile/NotificationSettings.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useContext, useEffect, useRef } from 'react';
 import { View, Switch } from 'react-native';
 
 // Context
@@ -19,7 +19,9 @@ export default NotificationSettings = ({ navigation }) => {
     const { setTabBarVisible } = useContext(FeedContext);
     useEffect(() => {
         setTabBarVisible(false);
-        return () => {setTabBarVisible(true)}
+        return () => {
+            setTabBarVisible(true)
+        }
     })
 
     const ViewContainer = styled(View)`
@@ -45,6 +47,7 @@ const NotificationsSettingsWrapper = ({ cognitoUser }) => {
     const [notifyPrompts, setNotifyPrompts] = useState(true);
     const [notifyReactions, setNotifyReactions] = useState(true);
     const [notifyTrending, setNotifyTrending] = useState(true);
+    const componentMounted = useRef(true);
 
     const NotificationSettingsContainer = styled(View)`
         width: 90%;
@@ -67,13 +70,19 @@ const NotificationsSettingsWrapper = ({ cognitoUser }) => {
                 settingsNotifyTrending 
             } = await getMyNotificationSettings(cognitoUser);
             
-            setNotifyPrompts(settingsNotifyPrompts);
-            setNotifyReactions(settingsNotifyReactions);
-            setNotifyTrending(settingsNotifyTrending);
-            const shouldSetNotifyAll = (settingsNotifyPrompts && settingsNotifyReactions && settingsNotifyTrending);
-            setNotifyAll(shouldSetNotifyAll);
+           if (componentMounted.current) {  // no react state updates on unmounted components since this is async setter
+                setNotifyPrompts(settingsNotifyPrompts);
+				setNotifyReactions(settingsNotifyReactions);
+				setNotifyTrending(settingsNotifyTrending);
+				const shouldSetNotifyAll =
+					settingsNotifyPrompts && settingsNotifyReactions && settingsNotifyTrending;
+				setNotifyAll(shouldSetNotifyAll);
+            }
        }
-       asyncGetNotificationSettings();
+        asyncGetNotificationSettings();
+        return () => {
+           componentMounted.current = false;
+       }
     }, [])
 
     /* 
