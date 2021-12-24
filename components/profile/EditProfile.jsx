356d013d14e5b301e8b15d77f3466d55c7286ccd
theurul <andras.zolyomi@gmail.com>
2021-12-24 12:38:20 +0100

    Modal for Profile Picture Upload complete, endpoint patch is WIP

--- components/profile/EditProfile.jsx
+++ components/profile/EditProfile.jsx
@@ -1,11 +1,19 @@
-import React, { useContext, useEffect } from "react";
-import { Modal, View, Text, Image, Pressable, SafeAreaView } from "react-native";
+import React, { useContext, useEffect, useState } from "react";
+import { Dimensions, Modal, View, Image, Pressable, SafeAreaView, Alert } from "react-native";
 import { FeedContext } from "../../context/FeedContext";
 import styled from "styled-components/native";
 import ReelayIcon from "../../assets/icons/reelay-icon.png";
 import * as ReelayText from "../global/Text";
 import ReelayColors from "../../constants/ReelayColors";
 import { HeaderDoneCancel } from '../global/Headers';
+import Constants from "expo-constants";
+import * as ImagePicker from "expo-image-picker";
+
+const { height, width } = Dimensions.get("window");
+
+const Spacer = styled(View)`
+	height: ${(props) => (props.height ? props.height : "0px")};
+`;
 
 export default EditProfile = ({ isEditingProfile, setIsEditingProfile }) => {
     const ModalContainer = styled(View)`
@@ -39,7 +47,7 @@ export default EditProfile = ({ isEditingProfile, setIsEditingProfile }) => {
 					<HeaderDoneCancel
 						withBar
 						onDone={doneFunc}
-						onCancel={doneFunc}
+						onCancel={cancelFunc}
 						text="Edit Profile"
 					/>
 					<EditProfileImage />
@@ -50,6 +58,11 @@ export default EditProfile = ({ isEditingProfile, setIsEditingProfile }) => {
 };
 
 const EditProfileImage = () => {
+    const [isEditingPhoto, setIsEditingPhoto] = useState(false);
+    
+    const startEditPhoto = () => {
+        setIsEditingPhoto(true);
+    }
 	const Container = styled(View)`
 		width: 100%;
 		padding-top: 20px;
@@ -58,7 +71,7 @@ const EditProfileImage = () => {
 		align-items: center;
 		justify-content: center;
 	`;
-	const EditContainer = styled(Pressable)`
+	const EditContainer = styled(View)`
 		display: flex;
 		flex-direction: column;
         align-items: center;
@@ -69,19 +82,157 @@ const EditProfileImage = () => {
 		width: 96px;
 		border-width: 2px;
 		border-color: white;
-		margin-bottom: 10px;
+		margin-bottom: 5px;
 	`;
 	const ProfileText = styled(ReelayText.Body2Bold)`
 		color: rgba(0, 165, 253, 1);
 		text-align: center;
+        padding: 5px;
 	`;
+    
 
-	return (
+    return (
 		<Container>
 			<EditContainer>
-				<ProfileImage source={ReelayIcon} />
-				<ProfileText>Change Profile Photo</ProfileText>
+				<Pressable onPress={startEditPhoto}>
+					<ProfileImage source={ReelayIcon} />
+				</Pressable>
+				<Pressable onPress={startEditPhoto}>
+					<ProfileText>Change Profile Photo</ProfileText>
+				</Pressable>
 			</EditContainer>
+			<EditingPhotoMenuModal
+				visible={isEditingPhoto}
+				close={() => {
+					setIsEditingPhoto(false);
+				}}
+			/>
 		</Container>
 	);
 };
+
+const EditingPhotoMenuModal = ({ visible, close }) => {
+
+    const [image, setImage] = useState(null);
+	const [percentage, setPercentage] = useState(0);
+
+    const takePhoto = async () => {
+        const cameraStatus = await ImagePicker.requestCameraPermissionsAsync();
+        if (cameraStatus.status !== "granted") {
+            alert("Please enable your camera permissions to take a photo.");
+            return;
+        }
+		let result = await ImagePicker.launchCameraAsync({
+			mediaTypes: "Images",
+			aspect: [4, 3],
+		});
+		handleImagePicked(result);
+	};
+
+    const choosePhoto = async () => {
+		const cameraRollStatus = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (cameraRollStatus.status !== "granted") {
+            alert("Please enable your camera roll permissions to select a photo.");
+            return;
+        }
+		let result = await ImagePicker.launchImageLibraryAsync({
+			mediaTypes: "Images",
+			aspect: [4, 3],
+			quality: 1,
+		});
+
+		handleImagePicked(result);
+	};;
+
+	handleImagePicked = async (pickerResult) => {
+        try {
+            console.log(pickerResult);
+			if (pickerResult.cancelled) {
+				alert("Upload cancelled");
+				return;
+			} else {
+				setPercentage(0);
+                const img = await fetchImageFromUri(pickerResult.uri);
+			}
+		} catch (e) {
+			console.log(e);
+			alert("Upload failed");
+		}
+	};
+
+	const fetchImageFromUri = async (uri) => {
+		const response = await fetch(uri);
+		const blob = await response.blob();
+		return blob;
+    };
+    
+    const ModalContainer = styled(View)`
+		position: absolute;
+	`;
+    const MenuContainer = styled(View)`
+        background: transparent;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin-top: auto;
+        margin-bottom: 40px;
+    `
+    const MenuOptionsContainer = styled(View)`
+        width: 90%;
+        background-color: #2D2D2D;
+        border-radius: 20px;
+        display: flex;
+        flex-direction: column;
+    `
+    const MenuOption = ({onPress, children}) => {
+        const MenuOptionComponent = styled(Pressable)`
+			width: 100%;
+			height: 60px;
+			justify-content: center;
+			align-items: center;
+			border-radius: 20px;
+		`;
+        return (
+            <MenuOptionComponent style={({ pressed }) => [
+								{ backgroundColor: pressed ? "#292929" : "#2D2D2D" },
+            ]} onPress={onPress}>
+                {children}
+            </MenuOptionComponent>
+        )
+    }
+    
+    const MenuOptionText = styled(ReelayText.Body1)`
+        color: ${ReelayColors.reelayBlue};
+        text-align: center;
+    `
+    const Backdrop = styled(Pressable)`
+		background-color: transparent;
+		height: 100%;
+		position: absolute;
+		width: 100%;
+	`;
+    return (
+		<ModalContainer>
+			<Modal animationType="slide" transparent={true} visible={visible}>
+				<Backdrop onPress={close} />
+				<MenuContainer>
+					<MenuOptionsContainer>
+						<MenuOption onPress={choosePhoto}>
+							<MenuOptionText>Choose Photo</MenuOptionText>
+						</MenuOption>
+						<MenuOption onPress={takePhoto}>
+							<MenuOptionText>Take Photo</MenuOptionText>
+						</MenuOption>
+					</MenuOptionsContainer>
+					<Spacer height="10px" />
+					<MenuOptionsContainer>
+						<MenuOption onPress={close}>
+							<MenuOptionText>Cancel</MenuOptionText>
+						</MenuOption>
+					</MenuOptionsContainer>
+				</MenuContainer>
+			</Modal>
+		</ModalContainer>
+	);
+}
