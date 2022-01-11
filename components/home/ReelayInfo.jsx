f53deb4b8ace0886a340352470fdef94dfadc881
Anthony J Mainero <anthony@Anthonys-MBP.lan>
2022-01-11 13:13:41 -0800

    Username and title on reelay feed use new fonts

--- components/home/ReelayInfo.jsx
+++ components/home/ReelayInfo.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { Dimensions, Pressable, Text, SafeAreaView, View } from 'react-native';
-import { AuthContext } from '../../context/AuthContext';
+import * as ReelayText from '../../components/global/Text';
 import styled from 'styled-components/native';
 import moment from 'moment';
 import { FeedContext } from '../../context/FeedContext';
@@ -19,38 +19,21 @@ export default ReelayInfo = ({ navigation, reelay }) => {
 		flex-direction: row;
 		align-items: center;
 	`
-	const Username = styled(Text)`
+	const Username = styled(ReelayText.Subtitle1Emphasized)`
 		align-self: flex-end;
-		font-size: 19px;
-		color: rgba(255, 255, 255, 1);
-		text-shadow: 3px 3px 3px rgba(0, 0, 0, 0.1);
-		font-weight: bold;
-		letter-spacing: -0.3px;
-	`
-	const Timestamp = styled(Text)`
-		align-self: flex-end;
-		color: rgba(255, 255, 255, 1.2);
-		font-size: 16px;
-		letter-spacing: -0.2px;
-		margin-left: 10px;
+		color: white;
 	`
 	const TitleInfo = styled(View)`
 		flex-direction: row;
 		align-items: center;
 	`
-	const Title = styled(Text)`
-		font-size: 18px;
-		color: rgba(255, 255, 255, 1.2);
-		letter-spacing: -0.2px;
-		margin-top: 6px;
+	const Title = styled(ReelayText.Body2)`
+		color: white;
 	`
 
 	const displayTitle = (reelay.title.display) ? reelay.title.display : 'Title not found\ ';
 	const year = (reelay.title.releaseYear) ? reelay.title.releaseYear : '';
 	const creator = reelay.creator;
-	const timestamp = moment(reelay.postedDateTime).fromNow();
-
-	const { setIsPaused } = useContext(FeedContext);
 
 	const goToProfile = () => {
 		navigation.push('UserProfileScreen', { creator });
@@ -61,7 +44,6 @@ export default ReelayInfo = ({ navigation, reelay }) => {
 			<Pressable onPress={goToProfile}>
 				<PostInfo>
 					<Username>@{creator?.username}</Username>
-					{/* <Timestamp>{timestamp}</Timestamp> */}
 				</PostInfo>
 				<TitleInfo>
 					<Title>{displayTitle} ({year})</Title>
