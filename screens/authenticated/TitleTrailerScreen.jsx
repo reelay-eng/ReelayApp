71572a5318a8a084bf7b237f918833dbb9072e4b
Anthony J Mainero <anthony@Anthonys-MBP.lan>
2022-01-12 18:30:20 -0800

    Trailer screen is slightly less glitchy

--- screens/authenticated/TitleTrailerScreen.jsx
+++ screens/authenticated/TitleTrailerScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import YoutubeVideoEmbed from '../../components/utils/YouTubeVideoEmbed';
 import { Dimensions, SafeAreaView, View, Pressable } from 'react-native';
 import { Icon } from 'react-native-elements';
@@ -7,19 +7,22 @@ import * as ReelayText from "../../components/global/Text";
 
 import * as ScreenOrientation from 'expo-screen-orientation';
 
-const { height } = Dimensions.get('window');
-const TRAILER_HEIGHT = height * 0.3;
+const { width } = Dimensions.get('window');
+const TRAILER_HEIGHT = width; // so that these proportions still work in landscape
 
 export default TitleTrailerScreen = ({navigation, route }) => {
     const { trailerURI} = route.params;
+
     const ViewContainer = styled(SafeAreaView)`
         background-color: #0d0d0d;
         align-items: center;
+        height: 100%;
+        width: 100%;
+        justify-content: center;
     `
     const TitleOverlayTrailerContainer = styled(View)`
         width: 100%;
-        margin-top: 50px;
-        height: ${height}px;
+        height: ${TRAILER_HEIGHT}px;
     `;
     const BackButtonContainer = styled(View)`
         position: absolute;
@@ -30,18 +33,13 @@ export default TitleTrailerScreen = ({navigation, route }) => {
         position: absolute;
         width: 100%;
         height: 100%;
-        top: ${TRAILER_HEIGHT/2}px;
+        display: flex;
+        justify-content: center;
 		align-items: center;
 	`;
     const LoadingText = styled(ReelayText.H6)`
         color: white;
-    `
-
-    ScreenOrientation.addOrientationChangeListener(() => {
-        console.log(Dimensions.get("window").height);
-        console.log('screen orientation changed');
-    });
-    
+    `    
     const onPress = async () => {
         console.log("orientation fix start");
 		await ScreenOrientation.lockAsync(
