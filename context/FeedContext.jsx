ff529288b8fea5a7ce61a3c7762aee5cee688662
Audrey Zhu <azhu@ucsb.edu>
2022-03-07 15:34:16 -0800

    Revert "cleaned up feed context"
    This reverts commit dfda02c2595aaf13123e39a431675bd6fb53a5ad.

--- context/FeedContext.jsx
+++ context/FeedContext.jsx
@@ -1,7 +1,10 @@
 import React, { createContext } from 'react';
 
 export const FeedContext = createContext({
+    commentsVisible: false,
     currentComment: '',
+    dotMenueVisible: false,
+    likesVisible: false,
     paused: false,
     playPauseVisible: 'none',
     tabBarVisible: true,
