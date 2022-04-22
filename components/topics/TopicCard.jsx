78d04a83152a9dc209994f285365cd74922fe3b9
Anthony J Mainero <anthony@Anthonys-MacBook-Pro-2.local>
2022-04-22 10:34:25 +1000

    Stubbed out bottom row when 0 reelays have been added to a topic

--- components/topics/TopicCard.jsx
+++ components/topics/TopicCard.jsx
@@ -6,14 +6,37 @@ import styled from 'styled-components/native';
 import ProfilePicture from '../global/ProfilePicture';
 import { Icon } from 'react-native-elements';
 import { LinearGradient } from "expo-linear-gradient";
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const { height, width } = Dimensions.get('window');
 
-const CardBottomLine = styled(View)`
-
+const BottomRowContainer = styled(View)`
+    align-items: center;
+    flex-direction: row;
+    justify-content: space-between;
+    margin: 16px;
+    bottom: 4px;
+`
+const BottomRowLeftText = styled(ReelayText.Subtitle2)`
+    color: #86878B;
 `
 const ContributorPicsContainer = styled(View)`
 
+`
+const CreateReelayButton = styled(TouchableOpacity)`
+    align-items: center;
+    background-color: #444950;
+    border-radius: 17px;
+    flex-direction: row;
+    justify-content: center;
+    height: 30px;
+    padding-left: 8px;
+    padding-right: 12px;
+`
+const CreateReelayText = styled(ReelayText.CaptionEmphasized)`
+    color: white;
+    font-size: 12px;
+    line-height: 16px;
 `
 const CreatorLine = styled(View)`
     align-items: center;
@@ -27,6 +50,8 @@ const CreatorName = styled(ReelayText.CaptionEmphasized)`
 `
 const DescriptionLine = styled(View)`
     margin-left: 18px;
+    margin-right: 18px;
+    margin-bottom: 9px;
 `
 const DescriptionText = styled(ReelayText.CaptionEmphasized)`
     color: #86878B;
@@ -41,7 +66,6 @@ const PromptText = styled(ReelayText.H5Emphasized)`
 const TopicCardContainer = styled(View)`
     background-color: black;
     border-radius: 11px;
-    height: 165px;
     margin-right: 8px;
     width: ${width-32}px;
 `
@@ -54,7 +78,22 @@ const TopicCardGradient = styled(LinearGradient)`
 const WatchReelaysButtonContainer = styled(View)`
 
 `
-export default TopicCard = ({ topic }) => {
+
+const CardBottomRowNoStacks = ({ topic, stacks }) => {
+    return (
+        <BottomRowContainer>
+            <BottomRowLeftText>
+                {'0 reelays, be the first!'}
+            </BottomRowLeftText>
+            <CreateReelayButton>
+                <Icon type='ionicon' name='add' color='white' size={20} />
+                <CreateReelayText>{'Create Reelay'}</CreateReelayText>
+            </CreateReelayButton>
+        </BottomRowContainer>
+    );
+}
+
+export default TopicCard = ({ topic, stacks = [] }) => {
     const creator = {
         sub: topic.creatorSub,
         username: topic.creatorUsername,
@@ -71,8 +110,9 @@ export default TopicCard = ({ topic }) => {
                 <PromptText>{topic.prompt}</PromptText>
             </PromptLine>
             <DescriptionLine>
-                <DescriptionText>{topic.description}</DescriptionText>
+                <DescriptionText numberOfLines={2}>{topic.description}</DescriptionText>
             </DescriptionLine>
+            { !stacks.length && <CardBottomRowNoStacks /> }
         </TopicCardContainer>
     );
 }
