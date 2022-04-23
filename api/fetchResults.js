c7dbd470bc46a3c87178471233fcc3375371ece9
Anthony J Mainero <anthony@Anthonys-MacBook-Pro-2.local>
2022-04-23 11:30:02 +1000

    Fetch timeout now always defaults to 30 seconds

--- api/fetchResults.js
+++ api/fetchResults.js
@@ -1,10 +1,6 @@
-import Constants from 'expo-constants';
-const FEED_VISIBILITY = Constants.manifest.extra.feedVisibility;
-const DEFAULT_TIMEOUT = (FEED_VISIBILITY === 'dev') ? 30000 : 10000;
-
 // https://dmitripavlutin.com/timeout-fetch-request/
-export const fetchResults = async (query, options={ timeout: DEFAULT_TIMEOUT }) => {
-    const { timeout = DEFAULT_TIMEOUT } = options;
+export const fetchResults = async (query, options={ timeout: 30000 }) => {
+    const { timeout = 30000 } = options;
 
     try {
         const controller = new AbortController();
