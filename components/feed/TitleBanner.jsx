f8c82b50b07bdf9598774ed3557cd9189dbeead5
Anthony J Mainero <anthony@Anthonys-MBP-2.lan>
2022-07-29 21:35:26 -0700

    Adds expandable title banner with artist info and a watch trailer button

--- components/feed/TitleBanner.jsx
+++ components/feed/TitleBanner.jsx
@@ -1,5 +1,5 @@
-import React, { memo, useContext } from "react";
-import { Dimensions, Pressable, SafeAreaView, View } from "react-native";
+import React, { Fragment, memo, useContext, useState } from "react";
+import { Dimensions, Pressable, SafeAreaView, TouchableOpacity, View } from "react-native";
 import * as ReelayText from '../global/Text';
 import { AuthContext } from "../../context/AuthContext";
 import Constants from 'expo-constants';
@@ -12,24 +12,78 @@ import styled from 'styled-components/native';
 import TitlePoster from "../global/TitlePoster";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import AddToStackButton from "./AddToStackButton";
+import { Icon } from "react-native-elements";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faChevronDown, faClapperboard, faPlay, faStar } from "@fortawesome/free-solid-svg-icons";
+import { getRuntimeString } from "../utils/TitleRuntime";
 
 const { height, width } = Dimensions.get('window');
 
-const TitleBannerOuterContainer = styled(SafeAreaView)`
-    margin-left: 10px;
-    position: ${props => props.absolute ? 'absolute' : 'relative'};
-    top: 20px;
+// a collapsed (!expanded) banner is 100px in height
+// the add to stack button is 45px
+const AddToStackButtonContainer = styled(View)`
+    top: 27.5px;
 `
-const TitleBannerContainer = styled(Pressable)`
-    align-self: center;
+const ArtistBadgeView = styled(View)`
+    align-items: center;
+    background-color: rgba(255,255,255,0.25);
+    border-radius: 8px;
+    justify-content: center;
+    margin-left: 8px;
+    padding: 8px;
+    display: flex;
+    flex-direction: row;
+`
+const ArtistRow = styled(View)`
+    align-items: center;
+    flex-direction: row;
+    margin-left: 8px;
+    padding-top: 8px;
+`
+const ArtistText = styled(ReelayText.CaptionEmphasized)`
+    color: white;
+    height: 16px;
+`
+const AllArtistsView = styled(Pressable)`
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+    padding: 12px;
+    padding-top: 0px;
+    width: 100%;
+`
+const ExpandableView = styled(View)`
+    align-items: center;
+    justify-content: center;
+    margin-top: 8px;
+    width: 100%;
+`
+const OverviewSpacer = styled(View)`
+    height: 8px;
+`
+const OverviewText = styled(ReelayText.CaptionEmphasized)`
+    color: white;
+    padding: 8px;
+`
+const RuntimeText = styled(ReelayText.CaptionEmphasized)`
+    color: white;
+    height: 16px;
+    margin-left: 12px;
+`
+const TitleBannerRow = styled(View)`
+    align-items: flex-start;
+    flex-direction: row;
+    justify-content: space-between;
+`
+const TitleBannerBackground = styled(Pressable)`
     background-color: ${props => props.color};
     border-radius: 8px;
+    margin-left: 10px;
+    top: 20px;
     width: ${width - 20}px;
-    justify-content: space-between;
-    flex-direction: row;
     zIndex: 3;
 `
-const TitleInfo = styled(View)`
+const TitleInfo = styled(Pressable)`
     align-items: flex-start;
     justify-content: center;
     font-size: 18px;
@@ -46,6 +100,7 @@ const TitleText = styled(ReelayText.H5Bold)`
     font-size: 18px;
 `
 const TitleTextContainer = styled(View)`
+    margin-top: 10px;
     justify-content: center;
     display: flex;
 `
@@ -56,6 +111,18 @@ const TitleUnderlineContainer = styled(View)`
 `
 const VenueContainer = styled(View)`
     margin-right: 5px;
+`
+const WatchTrailerPressable = styled(TouchableOpacity)`
+    align-items: center;
+    background-color: rgba(4, 189, 108, 0.5);
+    border-radius: 8px;
+    justify-content: center;
+    margin-left: 8px;
+    margin-right: 8px;
+    padding: 8px;
+    display: flex;
+    flex-direction: row;
+    width: 125px;
 `
 const YearText = styled(ReelayText.CaptionEmphasized)`
     color: white;
@@ -68,11 +135,60 @@ const YearVenueContainer = styled(View)`
 
 const DEFAULT_BGCOLOR = 'rgba(0, 0, 0, 0.36)';
 
+const ActorLine = ({ actorName }) => {
+    if (!actorName) return <View />;
+    return (
+        <ArtistRow>
+            <FontAwesomeIcon icon={faStar} color='white' size={18} />
+            <ArtistBadgeView>
+                <ArtistText>{actorName}</ArtistText>
+            </ArtistBadgeView>
+        </ArtistRow>
+    );
+}
+
+const DirectorLine = ({ directorName }) => {
+    if (!directorName) return <View />;
+    return (
+        <ArtistRow>
+            <FontAwesomeIcon icon={faClapperboard} color='white' size={18} />
+            <ArtistBadgeView>
+                <ArtistText>{directorName}</ArtistText>
+            </ArtistBadgeView>
+        </ArtistRow>
+    );
+}
+
+const WatchTrailerButton = ({ navigation, title }) => {
+    if (!title?.trailerURI) return <View />;
+    
+    const advanceToWatchTrailer = () => {
+        navigation.push("TitleTrailerScreen", {
+			trailerURI: title?.trailerURI,
+		});
+		logAmplitudeEventProd("watchTrailer", {
+			title: title?.display,
+			source: "poster",
+		});
+    }
+
+    return (
+        <ArtistRow>
+            <FontAwesomeIcon icon={faPlay} color='white' size={18} />
+            <WatchTrailerPressable onPress={advanceToWatchTrailer}>
+                <ArtistText>{'Watch Trailer'}</ArtistText>
+            </WatchTrailerPressable>
+        </ArtistRow>
+    );
+}
+
 const venuesEqual = (prevProps, nextProps) => {
     return prevProps.venue === nextProps.venue;
 }
 
-const TitleUnderline = memo(({ venue, displayYear }) => {
+const TitleUnderline = memo(({ venue, displayYear, runtime }) => {
+    console.log('runtime: ', runtime);
+    const runtimeString = runtime ? getRuntimeString(runtime) : '';
     return (
         <TitleUnderlineContainer>
             <YearVenueContainer>
@@ -81,7 +197,8 @@ const TitleUnderline = memo(({ venue, displayYear }) => {
                         <VenueIcon venue={venue} size={20} border={1} />
                     </VenueContainer>
                 }
-                { displayYear.length > 0 && <YearText>{displayYear}</YearText> }
+                { displayYear?.length > 0 && <YearText>{displayYear}</YearText> }
+                <RuntimeText>{runtimeString}</RuntimeText>
             </YearVenueContainer>
         </TitleUnderlineContainer>
     );
@@ -99,6 +216,7 @@ const TitleBanner = ({
     reelay=null, 
 }) => {
     const { reelayDBUser } = useContext(AuthContext);
+    const [expanded, setExpanded] = useState(false);
     const topOffset = useSafeAreaInsets().top;
     const welcomeReelaySub = Constants.manifest.extra.welcomeReelaySub;
     const isWelcomeReelay = reelay && (welcomeReelaySub === reelay?.sub);
@@ -126,31 +244,59 @@ const TitleBanner = ({
         });
     }
 
+    const ExpandableInfo = () => {
+        if (!expanded) {
+            return (
+                <ExpandableView>
+                    <FontAwesomeIcon icon={faChevronDown} color='white' size={16} />
+                </ExpandableView>
+            );
+        } else {
+            return <View />
+        }
+    }
+
     return (
-        <TitleBannerOuterContainer absolute={!!reelay} topOffset={reelay ? topOffset : 0}>
-            <TitleBannerContainer color={backgroundColor} onPress={onPress ?? openTitleDetail}>
+        <TitleBannerBackground color={backgroundColor} onPress={onPress ?? openTitleDetail}>
+            <TitleBannerRow>
                 <TitlePosterContainer>
                     <TitlePoster title={titleObj} onPress={openTitleDetail} width={posterWidth} />
                 </TitlePosterContainer>
-                <TitleInfo>
+                <TitleInfo onPress={() => setExpanded(!expanded)}>
                     <TitleTextContainer>
                         <TitleText numberOfLines={2} ellipsizeMode={"tail"}>
                             {displayTitle}
                         </TitleText>
                     </TitleTextContainer>
-                    <TitleUnderline venue={reelay?.content?.venue} displayYear={displayYear} />
+                    <TitleUnderline 
+                        displayYear={displayYear} 
+                        runtime={titleObj?.runtime}
+                        venue={reelay?.content?.venue} 
+                    />
+                    <ExpandableInfo />
                 </TitleInfo>
                 { !donateObj && (
-                    <AddToStackButton 
-                        navigation={navigation} 
-                        reelay={reelay} 
-                        club={club}
-                        topic={topic}
-                    />
+                    <AddToStackButtonContainer>
+                        <AddToStackButton 
+                            navigation={navigation} 
+                            reelay={reelay} 
+                            club={club}
+                            topic={topic}
+                        />
+                    </AddToStackButtonContainer>
                 )}
                 { donateObj && <DonateButton donateObj={donateObj} reelay={reelay} /> }
-            </TitleBannerContainer>    
-        </TitleBannerOuterContainer>
+            </TitleBannerRow>    
+            { expanded && (
+                <AllArtistsView onPress={() => setExpanded(false)}>
+                    <OverviewText>{titleObj?.overview}</OverviewText>
+                    <ActorLine actorName={titleObj?.displayActors[0]?.name} />
+                    <ActorLine actorName={titleObj?.displayActors[1]?.name} />
+                    <DirectorLine directorName={titleObj?.director?.name} />
+                    <WatchTrailerButton navigation={navigation} title={titleObj} />
+                </AllArtistsView>
+            )}
+        </TitleBannerBackground>
     );
 }
 
