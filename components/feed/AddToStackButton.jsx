b079e1e64dd4f0e66a56ba6ecb99ad943f9ed6c2
Anthony J Mainero <anthony@Anthonys-MBP-2.lan>
2022-07-31 23:09:02 -0700

    Add to stack button color now differs based on reelay sub

--- components/feed/AddToStackButton.jsx
+++ components/feed/AddToStackButton.jsx
@@ -9,14 +9,25 @@ import { useDispatch, useSelector } from 'react-redux';
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faAdd } from "@fortawesome/free-solid-svg-icons";
 import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import ReelayColors from '../../constants/ReelayColors';
+
+const userSubIsEven = (userSub) => {
+    const userSubAsInt = parseInt(userSub, 16);
+    return userSubAsInt % 2 === 0;
+}
+
+const getAddToStackButtonColor = (markedSeen, userSub) => {
+    if (userSubIsEven(userSub)) {
+        return (markedSeen) ? 'rgba(41, 119, 239, 0.40)' : 'rgba(255, 255, 255, 0.20)';
+    } else {
+        return ReelayColors.reelayBlue;
+    }
+}
 
 const ClubsButtonCircleContainer = styled(View)`
     align-items: center;
     align-self: center;
-    background: ${({ markedSeen }) => (markedSeen) 
-        ? 'rgba(41, 119, 239, 0.40)'
-        : 'rgba(255, 255, 255, 0.20)'
-    };
+    background: ${props => getAddToStackButtonColor(props.markedSeen, props.userSub)};
     border-radius: 50px;
     height: 45px;
     justify-content: center;
@@ -48,6 +59,21 @@ export default AddToStackButton = ({ navigation, reelay, club=null, topic=null }
         } else {
             advanceToVenueSelect();
         }
+
+        logAmplitudeEventProd('pressedAddReelayButton', {
+            buttonColor: userSubIsEven(reelayDBUser?.sub) 
+                ? 'white' 
+                : ReelayColors.reelayBlue,
+            creatorName: reelay?.creator?.username,
+            creatorSub: reelay?.creator?.sub,
+            username: reelayDBUser?.username,
+            userSub: reelayDBUser?.sub,
+            title: reelay?.title?.display,
+            club: club?.name ?? null,
+            clubID: club?.id ?? null,
+            topicID: topic?.id ?? null,
+            topic: topic?.title ?? null,
+        });
     }
 
     const showMeSignupIfGuest = () => {
@@ -60,7 +86,7 @@ export default AddToStackButton = ({ navigation, reelay, club=null, topic=null }
 
     return (
         <ClubsButtonOuterContainer onPress={advanceToCreateReelay}>
-            <ClubsButtonCircleContainer markedSeen={false}>
+            <ClubsButtonCircleContainer markedSeen={false} userSub={reelayDBUser?.sub}>
                 { (false) && <FontAwesomeIcon icon={faCheck} color='white' size={22}/> }
                 { (true) && <FontAwesomeIcon icon={faAdd} color='white' size={22}/> }
             </ClubsButtonCircleContainer>
