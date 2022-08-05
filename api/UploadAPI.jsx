9180574f3d2d739434c87088c2982e859ce22837
Anthony J Mainero <anthony@Anthonys-MBP-2.lan>
2022-08-05 12:16:10 -0700

    Remove attempt to compress video pre-upload

--- api/UploadAPI.jsx
+++ api/UploadAPI.jsx
@@ -147,12 +147,12 @@ const uploadReelayToS3 = async ({
     try {
         let uploadVideoURI = videoURI;
 
-        if (deviceCanCompress) {
-            const { error, outputURI, parsedSession } = await compressVideoForUpload(videoURI);
-            // console.log('parsed session: ', Object.keys(parsedSession));
-            // console.log('output uri: ', outputURI);
-            if (!error) uploadVideoURI = outputURI;
-        }
+        // if (deviceCanCompress) {
+        //     const { error, outputURI, parsedSession } = await compressVideoForUpload(videoURI);
+        //     // console.log('parsed session: ', Object.keys(parsedSession));
+        //     // console.log('output uri: ', outputURI);
+        //     if (!error) uploadVideoURI = outputURI;
+        // }
 
         // todo: get this working ^^
         
