9580a1859c7b578bb5c659b23210679eb02e9c5a
Anthony Mainero <anthony@reelay.app>
2022-10-20 11:42:45 +1000

    Adds draggability to clue rows, and advance to camera screen

--- screens/authenticated/CreateGuessingGameCluesScreen.jsx
+++ screens/authenticated/CreateGuessingGameCluesScreen.jsx
@@ -1,27 +1,46 @@
-import React from "react";
-import { Dimensions, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Dimensions, TouchableOpacity, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import * as ReelayText from '../../components/global/Text';
 import { HeaderWithBackButton } from "../../components/global/Headers";
 import styled from 'styled-components/native';
 import { getRuntimeString } from "../../components/utils/TitleRuntime";
 import TitlePoster from "../../components/global/TitlePoster";
-import { FlatList } from "react-native-gesture-handler";
+import { FlatList, ScrollView, TouchableWithoutFeedback } from "react-native-gesture-handler";
+import DraggableFlatList, { ScaleDecorator } from 'react-native-draggable-flatlist';
+import ReelayColors from "../../constants/ReelayColors";
+import { LogBox } from 'react-native';
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
 
 const { width } = Dimensions.get('window');
 
 const ActorText = styled(ReelayText.Subtitle2)`
     color: gray
 `
+const AddReelayButtonView = styled(View)`
+    align-items: center;
+    height: 100%;
+    justify-content: center;
+    padding-left: 16px;
+    padding-right: 16px;
+`
+const ClueReelayDescriptionText = styled(ReelayText.Body1)`
+    align-items: flex-end;
+    color: white;
+    flex: 1;
+    flex-direction: row;
+    text-align: left;
+`
 const ClueReelayDescriptionView = styled(View)`
     align-items: center;
-    background-color: #252527;
+    background-color: ${props => props.backgroundColor};
     border-radius: 10px;
     display: flex;
     flex: 1;
     flex-direction: row;
     height: 100%;
-    padding: 16px;
+    justify-content: flex-end;
 `
 const ClueReelayIndexText = styled(ReelayText.H5Bold)`
     color: white;
@@ -32,13 +51,19 @@ const ClueReelayIndexView = styled(View)`
     margin-right: 12px;
     width: 24px;
 `
-const ClueReelayRowView = styled(View)`
+const ClueReelayRowPressable = styled(TouchableWithoutFeedback)`
     align-items: center;
     flex-direction: row;
     height: 82px;
     margin-top: 8px;
     margin-bottom: 8px;
+    padding-left: 16px;
+    padding-right: 16px;
     width: 100%;
+`
+const CluesHeaderView = styled(View)`
+    padding: 16px;
+    padding-top: 20px;
 `
 const CluesSectionView = styled(View)`
     padding: 16px;
@@ -90,8 +115,6 @@ const YearText = styled(ReelayText.Subtitle2)`
     color: gray
 `
 
-
-
 export default CreateGuessingGameCluesScreen = ({ navigation, route }) => {
     const topOffset = useSafeAreaInsets().top;
     const bottomOffset = useSafeAreaInsets().bottom;
@@ -110,29 +133,49 @@ export default CreateGuessingGameCluesScreen = ({ navigation, route }) => {
         ? correctTitleObj.releaseDate.slice(0,4) : '';
     const runtimeString = getRuntimeString(correctTitleObj?.runtime);
 
-    const ClueReelayRow = ({ item, index }) => {
+    const [clues, setClues] = useState([null]);
+
+    useEffect(() => {
+        LogBox.ignoreLogs(['VirtualizedLists should never be nested']);
+    }, []);
+
+    const ClueReelayDescription = ({ isActive, reelay = null }) => {
+        const backgroundColor = (isActive) 
+            ? ReelayColors.reelayBlue
+            : (!!reelay) ? '#252527' : 'black';
+
+        if (!reelay) {
+            return (
+                <ClueReelayDescriptionView isActive={isActive} backgroundColor={backgroundColor}>
+                    <ClueReelayDescriptionText>{'Add a video clue...'}</ClueReelayDescriptionText>
+                    <AddReelayButtonView>
+                        <FontAwesomeIcon icon={faPlusCircle} size={30} color='white' />
+                    </AddReelayButtonView>
+                </ClueReelayDescriptionView>
+            );    
+        } else {
+            return (
+                <ClueReelayDescriptionView isActive={isActive} backgroundColor={backgroundColor}>
+    
+                </ClueReelayDescriptionView>
+            );    
+        }
+    }
+
+    const ClueReelayRow = ({ item, index, drag, isActive }) => {
+        const advanceToCreateReelayScreen = () => navigation.push('ReelayCameraScreen', {
+            titleObj: correctTitleObj
+        })
+
+        const reelay = item;
+        const onPress = (reelay) ? () => {} : advanceToCreateReelayScreen;
         return (
-            <ClueReelayRowView>
+            <ClueReelayRowPressable delayLongPress={250} onPress={onPress} onLongPress={drag} disabled={isActive}>
                 <ClueReelayIndexView>
                     <ClueReelayIndexText>{index + 1}</ClueReelayIndexText>
                 </ClueReelayIndexView>
-                <ClueReelayDescriptionView />
-            </ClueReelayRowView>
-        );
-    }
-
-    const CluesSection = () => {
-        return (
-            <CluesSectionView>
-                <HeaderText>{'Clues'}</HeaderText>
-                <HeaderSubText>{'Add reelays to help people guess the title. Players get one guess for each reelay they see.'}</HeaderSubText>
-                <FlatList
-                    data={[1,2,3,4,5,6]}
-                    renderItem={({ item, index }) => <ClueReelayRow item={item} index={index} /> }                
-                    showsVerticalScrollIndicator={false}
-                    style={{ height: '50%'}}
-                />
-            </CluesSectionView>
+                <ClueReelayDescription isActive={isActive} />
+            </ClueReelayRowPressable>
         );
     }
 
@@ -154,14 +197,40 @@ export default CreateGuessingGameCluesScreen = ({ navigation, route }) => {
         );
     }
 
+    const ComponentAboveList = () => {
+        return (
+            <View>
+                <HeaderWithBackButton navigation={navigation} text={'guessing game'} />
+                <GameTitleView>
+                    <GameTitleText>{gameTitle}</GameTitleText>
+                </GameTitleView>
+                <CorrectTitleLine />
+                <CluesHeaderView>
+                    <HeaderText>{'Clues'}</HeaderText>
+                    <HeaderSubText>{'Add reelays to help people guess the title. Players get one guess for each reelay they see.'}</HeaderSubText>
+                    <HeaderSubText>{'Press and hold to reorder.'}</HeaderSubText>
+                </CluesHeaderView>
+            </View>
+        );
+    }
+
+    const ComponentBelowList = () => {
+        return (
+            <View style={{ height: 200 }} />
+        )
+    }
+
     return (
         <ScreenView topOffset={topOffset}>
-            <HeaderWithBackButton navigation={navigation} text={'guessing game'} />
-            <GameTitleView>
-                <GameTitleText>{gameTitle}</GameTitleText>
-            </GameTitleView>
-            <CorrectTitleLine />
-            <CluesSection />
+            <DraggableFlatList
+                ListHeaderComponent={ComponentAboveList}
+                ListFooterComponent={ComponentBelowList}
+                data={clues}
+                keyExtractor={(item, index) => (!!item) ? item?.id : index}
+                onDragEnd={({ data }) => setClues(data)}
+                renderItem={(props) => <ClueReelayRow { ...props } /> }                
+                showsVerticalScrollIndicator={false}
+            />
         </ScreenView>
     )
 }
