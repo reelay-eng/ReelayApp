0a67fdacba03bdf4094c2e47471c04ca458208b6
Anthony Mainero <anthony@reelay.app>
2022-10-21 11:34:42 +1000

    Guessing game API matches first draft of server side endpoints

--- api/GuessingGameApi.jsx
+++ api/GuessingGameApi.jsx
@@ -1,60 +1,24 @@
 import Constants from 'expo-constants';
 import { fetchResults } from './fetchResults';
-import ReelayAPIHeaders, { getReelayAuthHeaders } from './ReelayAPIHeaders';
+import { getReelayAuthHeaders } from './ReelayAPIHeaders';
 import { prepareReelay } from './ReelayDBApi';
 
 const FEED_VISIBILITY = Constants.manifest.extra.feedVisibility;
 const REELAY_API_BASE_URL = Constants.manifest.extra.reelayApiBaseUrl;
 
-export const addClueToGuessingGame = async ({
-    authSession,
-    reelayDBBody,
-    reqUserSub,
-    topicID,
-}) => {
-    // TODO
-}
-
-export const addResponseReelayToGuessingGame = async ({
-    authSession,
-    reelayDBBody,
-    reqUserSub,
-    topicID,
-}) => {
-    // TODO
-}
-
-export const createDraftGuessingGame = async ({
-    authSession,
-    clubID = null,
-    correctTitleKey,
-    creatorName, 
-    creatorSub, 
-    title, 
-}) => {
-    const routePost = `${REELAY_API_BASE_URL}/guessingGame/draft`;
-    const postBody = {
-        clubID,
-        correctTitleKey,
-        creatorName,
-        creatorSub,
-        title,
-        visibility: FEED_VISIBILITY,
-    };
-
-    const createDraftGuessingGameResult = await fetchResults(routePost, {
-        method: 'POST',
-        headers: {
-            ...getReelayAuthHeaders(authSession),
-            requsersub: creatorSub,
-        },
-        body: JSON.stringify(postBody),
-    });
-    return createDraftGuessingGameResult;
+const prepareGame = async (guessingGame) => {
+    try {
+        guessingGame.details = JSON.parse(guessingGame?.detailsJSON);
+        guessingGame.reelays = await Promise.all(guessingGame.reelays.map(prepareReelay));
+    } catch (error) {
+        guessingGame.details = { error: 'Could not parse details JSON'};
+        guessingGame.reelays = [];
+    }
+    return guessingGame;
 }
 
 export const getMyDraftGuessingGames = async ({ authSession, reqUserSub }) => {
-    const routeGet = `${REELAY_API_BASE_URL}/guessingGame/draft?page=${page}`;
+    const routeGet = `${REELAY_API_BASE_URL}/guessingGame/drafts`;
     const fetchedGames = await fetchResults(routeGet, {
         method: 'GET',
         headers: {
@@ -63,113 +27,302 @@ export const getMyDraftGuessingGames = async ({ authSession, reqUserSub }) => {
         },
     });
 
-    const prepareGameReelays = async (guessingGame) => {
-        guessingGame.reelays = await Promise.all(guessingGame.reelays.map(prepareReelay));
-        return guessingGame;
-    }
-    const preparedGames = await Promise.all(fetchedGames.map(prepareGameReelays));
+    const preparedGames = await Promise.all(fetchedGames.map(prepareGame));
     return preparedGames;
 }
 
-export const getPublishedGuessingGames = async ({ authSession, page = 0, reqUserSub }) => {
-    const routeGet = `${REELAY_API_BASE_URL}/guessingGame/published?page=${page}&visibility=${FEED_VISIBILITY}`;
+export const getGuessingGamesPlayedBy = async ({ authSession, reqUserSub, userSub }) => {
+    const routeGet = `${REELAY_API_BASE_URL}/guessingGame/playedBy?visibility=${FEED_VISIBILITY}`;
     const fetchedGames = await fetchResults(routeGet, {
         method: 'GET',
         headers: {
             ...getReelayAuthHeaders(authSession),
             requsersub: reqUserSub,
+            usersub: userSub,
         },
     });
 
-    const prepareGameReelays = async (guessingGame) => {
-        guessingGame.reelays = await Promise.all(guessingGame.reelays.map(prepareReelay));
-        return guessingGame;
-    }
-    const preparedGames = await Promise.all(fetchedGames.map(prepareGameReelays));
+    const preparedGames = await Promise.all(fetchedGames.map(prepareGame));
     return preparedGames;
 }
 
-export const getReportedGuessingGames = async ({ authSession, reqUserSub }) => {
-    const routeGet = `${REELAY_API_BASE_URL}/reported-content/guessingGame?visibility=${FEED_VISIBILITY}`;
-    const fetchedReportedGuessingGames = await fetchResults(routeGet, {
+export const getGuessingGamesPublished = async ({ 
+    authSession, 
+    reqUserSub, 
+    clubID = null,
+    creatorSub = null, 
+}) => {
+    const routeGet = `${REELAY_API_BASE_URL}/guessingGame/published?visibility=${FEED_VISIBILITY}`;
+    const fetchedGames = await fetchResults(routeGet, {
         method: 'GET',
-        headers: { 
-            ...getReelayAuthHeaders(authSession), 
-            requsersub: reqUserSub 
+        headers: {
+            ...getReelayAuthHeaders(authSession),
+            requsersub: reqUserSub,
+            creatorsub: creatorSub,
+            clubid: clubID,
         },
     });
 
-    const prepareGameReelays = async (guessingGame) => {
-        guessingGame.reelays = await Promise.all(guessingGame.reelays.map(prepareReelay));
-        return guessingGame;
-    }
-    const preparedGames = await Promise.all(fetchedReportedGuessingGames.map(prepareGameReelays));
+    const preparedGames = await Promise.all(fetchedGames.map(prepareGame));
     return preparedGames;
 }
 
-export const deleteGuessingGame = async ({ authSession, reqUserSub, topicID }) => {
-    const routeDelete =  `${REELAY_API_BASE_URL}/guessingGame`;
-    const deleteBody = { topicID };
-    const resultDelete = await fetchResults(routeDelete, {
+export const postGuessingGameClue = async ({
+    authSession,
+    reelaySub,
+    reqUserSub,
+    topicID,
+}) => {
+    const routePost = `${REELAY_API_BASE_URL}/guessingGame/clue`;
+    const postBody = { reelaySub, topicID };
+
+    const postGuessingGameClueResult = await fetchResults(routePost, {
+        method: 'POST',
+        headers: {
+            ...getReelayAuthHeaders(authSession),
+            requsersub: reqUserSub,
+        },
+        body: JSON.stringify(postBody),
+    });
+    return postGuessingGameClueResult;
+}
+
+export const postGuessingGameDraft = async ({
+    authSession,
+    reqUserSub,
+    clubID,
+    correctTitleKey,
+    creatorName,
+    title,
+}) => {
+    const routePost = `${REELAY_API_BASE_URL}/guessingGame/draft`;
+    const postBody = { 
+        clubID, 
+        correctTitleKey,
+        creatorName,
+        title,
+    };
+
+    const postGuessingGameDraftResult = await fetchResults(routePost, {
+        method: 'POST',
+        headers: {
+            ...getReelayAuthHeaders(authSession),
+            requsersub: reqUserSub,
+        },
+        body: JSON.stringify(postBody),
+    });
+    return postGuessingGameDraftResult;
+}
+
+export const postGuessingGameGuess = async ({
+    authSession,
+    reqUserSub,
+    clueIndex,
+    guessedTitleKey,
+    reelaySub,
+    topicID,
+}) => {
+    const routePost = `${REELAY_API_BASE_URL}/guessingGame/draft`;
+    const postBody = { 
+        clueIndex, 
+        guessedTitleKey,
+        reelaySub,
+        topicID,
+        visibility: FEED_VISIBILITY,
+    };
+
+    const postGuessingGameGuessResult = await fetchResults(routePost, {
+        method: 'POST',
+        headers: {
+            ...getReelayAuthHeaders(authSession),
+            requsersub: reqUserSub,
+        },
+        body: JSON.stringify(postBody),
+    });
+    return postGuessingGameGuessResult;
+}
+
+export const publishGuessingGame = async ({
+    authSession,
+    reqUserSub,
+    topicID,
+}) => {
+    const routePost = `${REELAY_API_BASE_URL}/guessingGame/publish`;
+    const postBody = { 
+        topicID,
+        visibility: FEED_VISIBILITY,
+    };
+
+    const publishGuessingGameResult = await fetchResults(routePost, {
+        method: 'POST',
+        headers: {
+            ...getReelayAuthHeaders(authSession),
+            requsersub: reqUserSub,
+        },
+        body: JSON.stringify(postBody),
+    });
+    return publishGuessingGameResult;
+}
+
+export const postGuessingGameReactionReelay = async ({
+    authSession,
+    reqUserSub,
+    reelaySub,
+    topicID,
+}) => {
+    const routePost = `${REELAY_API_BASE_URL}/guessingGame/draft`;
+    const postBody = { 
+        reelaySub,
+        topicID,
+        visibility: FEED_VISIBILITY,
+    };
+
+    const postGuessingGameReactionResult = await fetchResults(routePost, {
+        method: 'POST',
+        headers: {
+            ...getReelayAuthHeaders(authSession),
+            requsersub: reqUserSub,
+        },
+        body: JSON.stringify(postBody),
+    });
+    return postGuessingGameReactionResult;
+}
+
+export const patchGuessingGameDetails = async ({
+    authSession,
+    reqUserSub,
+    detailsJSON,
+    topicID,
+}) => {
+    const routePatch = `${REELAY_API_BASE_URL}/guessingGame/details`;
+    const patchBody = { detailsJSON, topicID };
+
+    const patchGuessingGameDetailsResult = await fetchResults(routePatch, {
+        method: 'PATCH',
+        headers: {
+            ...getReelayAuthHeaders(authSession),
+            requsersub: reqUserSub,
+        },
+        body: JSON.stringify(patchBody),
+    });
+    return patchGuessingGameDetailsResult;
+}
+
+export const patchGuessingGameTitle = async ({
+    authSession,
+    reqUserSub,
+    title,
+    topicID,
+}) => {
+    const routePatch = `${REELAY_API_BASE_URL}/guessingGame/title`;
+    const patchBody = { title, topicID };
+
+    const patchGuessingGameTitleResult = await fetchResults(routePatch, {
+        method: 'PATCH',
+        headers: {
+            ...getReelayAuthHeaders(authSession),
+            requsersub: reqUserSub,
+        },
+        body: JSON.stringify(patchBody),
+    });
+    return patchGuessingGameTitleResult;
+}
+
+export const patchGuessingGameVisibility = async ({
+    authSession,
+    reqUserSub,
+    topicID,
+    visibility
+}) => {
+    const routePatch = `${REELAY_API_BASE_URL}/guessingGame/visibility`;
+    const patchBody = { visibility, topicID };
+
+    const patchGuessingGameVisibilityResult = await fetchResults(routePatch, {
+        method: 'PATCH',
+        headers: {
+            ...getReelayAuthHeaders(authSession),
+            requsersub: reqUserSub,
+        },
+        body: JSON.stringify(patchBody),
+    });
+    return patchGuessingGameVisibilityResult;
+}
+
+export const deleteGuessingGameClue = async ({
+    authSession,
+    reqUserSub,
+    reelaySub,
+    topicID,
+}) => {
+    const routeDelete = `${REELAY_API_BASE_URL}/guessingGame/clue`;
+    const deleteBody = { reelaySub, topicID };
+
+    const deleteGuessingGameClueResult = await fetchResults(routeDelete, {
         method: 'DELETE',
-        headers: { 
-            ...getReelayAuthHeaders(authSession), 
-            requsersub: reqUserSub 
+        headers: {
+            ...getReelayAuthHeaders(authSession),
+            requsersub: reqUserSub,
         },
         body: JSON.stringify(deleteBody),
     });
-    return resultDelete;
+    return deleteGuessingGameClueResult;
 }
 
-export const publishGuessingGame = async ({ authSession, reqUserSub, topicID }) => {
-    const routePost = `${REELAY_API_BASE_URL}/guessingGame/publish`;
-    const postBody = { topicID };
-
-    const publishResult = await fetchResults(routePost, {
-        method: 'POST',
-        headers: {
-            ...getReelayAuthHeaders(authSession),
-            requsersub: reqUserSub,
-        },
-        body: JSON.stringify(postBody),
-    });
-    return publishResult;
-}
-
-export const reportGuessingGame = async ({ authSession, reqUserSub, reportReq }) => {
-    const routePost = `${REELAY_API_BASE_URL}/reported-content/guessingGame`;
-    const reportTopicResult = await fetchResults(routePost, {
-        body: JSON.stringify(reportReq),
-        method: 'POST',
-        headers: { 
-            ...getReelayAuthHeaders(authSession),
-            requsersub: reqUserSub,
-        },
-    });
-
-    console.log(reportTopicResult);
-    return reportTopicResult;
-}
-
-export const submitGuessingGameGuess = async ({ 
-    authSession, 
-    clueIndex, 
-    clueReelaySub, 
-    guessedTitleKey, 
-    reqUserSub, 
+export const deleteGuessingGameDraft = async ({
+    authSession,
+    reqUserSub,
     topicID,
 }) => {
-    const routePost = `${REELAY_API_BASE_URL}/guessingGame/guess`;
-    const postBody = { clueIndex, clueReelaySub, guessedTitleKey, topicID };
+    const routeDelete = `${REELAY_API_BASE_URL}/guessingGame/draft`;
+    const deleteBody = { topicID };
 
-    const submitGuessResult = await fetchResults(routePost, {
-        method: 'POST',
+    const deleteGuessingGameDraftResult = await fetchResults(routeDelete, {
+        method: 'DELETE',
         headers: {
             ...getReelayAuthHeaders(authSession),
             requsersub: reqUserSub,
         },
-        body: JSON.stringify(postBody),
+        body: JSON.stringify(deleteBody),
     });
-    return submitGuessResult;
+    return deleteGuessingGameDraftResult;
+}
+
+export const deleteGuessingGameGuess = async ({
+    authSession,
+    reqUserSub,
+    guessID,
+    topicID,
+}) => {
+    const routeDelete = `${REELAY_API_BASE_URL}/guessingGame/guess`;
+    const deleteBody = { guessID, topicID };
+
+    const deleteGuessingGameGuessResult = await fetchResults(routeDelete, {
+        method: 'DELETE',
+        headers: {
+            ...getReelayAuthHeaders(authSession),
+            requsersub: reqUserSub,
+        },
+        body: JSON.stringify(deleteBody),
+    });
+    return deleteGuessingGameGuessResult;
+}
+
+export const deleteGuessingGamePublished = async ({
+    authSession,
+    reqUserSub,
+    topicID,
+}) => {
+    const routeDelete = `${REELAY_API_BASE_URL}/guessingGame/published`;
+    const deleteBody = { guessID, topicID };
+
+    const deleteGuessingGamePublishedResult = await fetchResults(routeDelete, {
+        method: 'DELETE',
+        headers: {
+            ...getReelayAuthHeaders(authSession),
+            requsersub: reqUserSub,
+        },
+        body: JSON.stringify(deleteBody),
+    });
+    return deleteGuessingGamePublishedResult;
 }
 
