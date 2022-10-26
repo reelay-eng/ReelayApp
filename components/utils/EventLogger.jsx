7339dd4c9df8829490cf39845420e450b8d0d66f
Anthony Mainero <anthony@reelay.app>
2022-10-26 12:06:59 -0700

    Removes onRefresh when leaving club, preventing some weird errors

--- components/utils/EventLogger.jsx
+++ components/utils/EventLogger.jsx
@@ -1,30 +1,30 @@
 import { Amplitude } from '@amplitude/react-native';
 import Constants from 'expo-constants';
-import { Mixpanel } from 'mixpanel-react-native';
+// import { Mixpanel } from 'mixpanel-react-native';
 
 const canUseNativeModules = Constants.appOwnership !== 'expo';
 const FEED_VISIBILITY = Constants.manifest.extra.feedVisibility;
 
-let mixpanel;
+// let mixpanel;
 
 export const initMixpanel = (token) => {
-    mixpanel = new Mixpanel(token, trackAutomaticEvents=true);
-    mixpanel.init().then(() => {
-        console.log("[Mixpanel] Initialized");
-        // mixpanel.setUseIpAddressForGeolocation(false);
-    }).catch((e) => {
-        console.log("[Mixpanel] Initialization Failed: ", e);
-    });
+    // mixpanel = new Mixpanel(token, trackAutomaticEvents=true);
+    // mixpanel.init().then(() => {
+    //     console.log("[Mixpanel] Initialized");
+    //     // mixpanel.setUseIpAddressForGeolocation(false);
+    // }).catch((e) => {
+    //     console.log("[Mixpanel] Initialization Failed: ", e);
+    // });
 
 }
 
 export const identifyUser = ({ userSub, username }) => {
-    mixpanel.identify(userSub);
-    // properties that will never change
-    mixpanel.getPeople().setOnce({"userSub": userSub});
-    // properties that can change between/during sessions
-    mixpanel.getPeople().set({"$name": username});
-    console.log(`[Mixpanel] User ${username} identified by sub: [${userSub}]`);
+    // mixpanel.identify(userSub);
+    // // properties that will never change
+    // mixpanel.getPeople().setOnce({"userSub": userSub});
+    // // properties that can change between/during sessions
+    // mixpanel.getPeople().set({"$name": username});
+    // console.log(`[Mixpanel] User ${username} identified by sub: [${userSub}]`);
 }
 
 export const logAmplitudeEventProd = async (eventName, options) => {
@@ -37,6 +37,6 @@ export const logAmplitudeEventProd = async (eventName, options) => {
             disableIPAddress: true,
         }
         await Amplitude.getInstance('amp-reelay').logEvent(eventName, optionsNoLocation);
-        await mixpanel.track(eventName, options);
+        // await mixpanel.track(eventName, options);
     }
 }
