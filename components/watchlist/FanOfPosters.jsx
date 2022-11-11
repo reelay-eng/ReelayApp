7e72736a44d4686f33fd207e4067683717426c66
Anthony Mainero <anthony@reelay.app>
2022-11-11 12:38:23 -0800

    Adds empty titles to watchlist card

--- components/watchlist/FanOfPosters.jsx
+++ components/watchlist/FanOfPosters.jsx
@@ -1,6 +1,8 @@
+import { LinearGradient } from 'expo-linear-gradient';
 import React from 'react';
 import { View } from 'react-native';
 import styled from 'styled-components/native';
+import ReelayColors from '../../constants/ReelayColors';
 import TitlePoster from '../global/TitlePoster';
 
 // + 12, -12
@@ -27,6 +29,20 @@ const PosterPositions = [
     },
 ]
 
+const PosterFillEmpty = styled(View)`
+    background-color: #1B2B3D;
+    border-radius: 12px;
+    height: 100%;
+    opacity: 0.7;
+    position: absolute;
+    width: 100%;
+`
+const PosterGradientEmpty = styled(LinearGradient)`
+    border-radius: 12px;
+    height: 100%;
+    position: absolute;
+    width: 100%;
+`
 const PosterTiltView = styled(View)`
     left: ${props => props.left}px;
     position: absolute;
@@ -36,6 +52,14 @@ const PosterTiltView = styled(View)`
     top: ${props => props.top}px;
     transform: rotate(${props => props.tilt}deg);
 `
+const PosterTiltViewEmpty = styled(PosterTiltView)`
+    background-color: rbga(26,139,142,1);
+    border-color: white;
+    border-radius: 12px;
+    border-width: 1px;
+    height: 120px;
+    width: 80px;
+`
 const PosterRowView = styled(View)`
     flex-direction: row;
     height: 190px;
@@ -43,12 +67,28 @@ const PosterRowView = styled(View)`
 `
 
 export default FanOfPosters = ({ titles = [] }) => {
-    const displayTitles = (titles.length) > 4 
-        ? titles.slice(0,4) 
-        : titles;
+
+    const getDisplayTitles = () => {
+        const displayTitles = (titles.length) > 4 
+            ? [...titles.slice(0,4)] 
+            : [...titles];
+        while (displayTitles.length < 4) displayTitles.push(null);
+        return displayTitles.reverse();
+    }
 
     const renderAngledPoster = (title, index) => {
         const posterTiltProps = PosterPositions[index];
+        if (!title) {
+            return <PosterTiltViewEmpty {...posterTiltProps} key={index + 100}>
+                <PosterFillEmpty />
+                <PosterGradientEmpty 
+                    colors={['rgba(255,255,255,0.4)', 'rgba(255,255,255,0.1)']} 
+                    start={{x: 0, y: 0}}
+                    end={{x:1, y: 1}}
+                 />                
+            </PosterTiltViewEmpty>;
+        }
+
         return (
             <PosterTiltView {...posterTiltProps} key={title?.id}>
                 <TitlePoster title={title} width={80} />
@@ -58,7 +98,7 @@ export default FanOfPosters = ({ titles = [] }) => {
 
     return (
         <PosterRowView>
-             { displayTitles.map(renderAngledPoster) }
+             { getDisplayTitles().map(renderAngledPoster) }
         </PosterRowView>
     )
 }
