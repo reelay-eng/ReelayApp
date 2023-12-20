9f341c4f051205bdef6e52771b9d92698db172b3
tejaspatil31 <tpatil@technosofteng.com>
2023-12-20 13:42:13 +0530

    changes

--- components/home/SectionDiscover.jsx
+++ components/home/SectionDiscover.jsx
@@ -269,10 +269,10 @@ const SectionDiscover = ({ navigation, route, refreshControl }) => {
     const followingReels = useSelector((state) => state.followingReels);
     const watchlistReels = useSelector((state) => state.watchlistReels);
     const moreFiltersReels = useSelector((state) => state.moreFiltersReels);
-    // ---- TO DO -----
-    // const isItemAddedToWatchList = useSelector(
-    //   (state) => state.isItemAddedToWatchList
-    // );
+
+    const isItemAddedToWatchList = useSelector(
+      (state) => state.isItemAddedToWatchList
+    );
 
     const sortedThreadData = {
       mostRecent: discoverMostRecent,
